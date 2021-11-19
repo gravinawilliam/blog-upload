3540ec754d64b691d4a0fba1ff53dfcab448746a
William Gravina <dev.gravinawilliam@gmail.com>
2021-11-19 13:45:47 -0300

    refactored cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -26,7 +26,7 @@ Sentry.init({
 
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
-app.use(cors({}));
+app.use(cors({ origin: '*' }));
 
 app.use(express.json());
 app.use(routes);
