dc6081a7bd3cef2f87dae69556a1a70ef467b811
William Gravina <dev.gravinawilliam@gmail.com>
2021-11-19 05:50:31 -0300

    refactor typeorm config

--- ormconfig.js
+++ ormconfig.js
@@ -23,12 +23,12 @@ const devConfig = [
 const prodConfig = [
   {
     name: 'default',
-    type: process.env.DEFAULT_TYPE,
-    host: process.env.DEFAULT_HOST,
-    port: process.env.DEFAULT_PORT,
-    username: process.env.DEFAULT_USERNAME,
-    password: process.env.DEFAULT_PASSWORD,
-    database: process.env.DEFAULT_DB_NAME,
+    type: process.env.DB_DEFAULT_TYPE,
+    host: process.env.DB_DEFAULT_HOST,
+    port: process.env.DB_DEFAULT_PORT,
+    username: process.env.DB_DEFAULT_USERNAME,
+    password: process.env.DB_DEFAULT_PASSWORD,
+    database: process.env.DB_DEFAULT_DB_NAME,
     entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
     migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
     cli: {
